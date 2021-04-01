65959e6940ed2f7d0642943402181c46f1674dfe
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-04-01 19:18:10 +0200

    2.3: Course information step8

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,5 +1,6 @@
 const Total = ({ parts }) => {
   const exercises = parts.map(({ exercises }) => exercises)
+
   return (
     <strong>
       total of {exercises.reduce((acc, val) => acc + val)} exercises
