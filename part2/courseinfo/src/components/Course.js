97ba632bba2910138750f00da7f8a001ee0ab209
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-04-01 19:21:47 +0200

    2.5: separate module

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,14 +2,12 @@ import Content from './Content'
 import Header from './Header'
 import Total from './Total'
 
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
 
 export default Course
