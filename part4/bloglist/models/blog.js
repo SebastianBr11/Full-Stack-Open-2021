8d4bcf4f275490cd480e3651a75648397e87493d
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-06-28 09:02:04 +0200

    4.2 Blog list, step2

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
+module.exports = Blog
