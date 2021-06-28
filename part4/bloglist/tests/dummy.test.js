f9ae8eea0512402513edca199f94db37f947f73e
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-06-28 09:26:29 +0200

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -80,3 +80,67 @@ describe('total likes', () => {
     expect(result).toBe(5)
   })
 })
+
+describe('favorite blog', () => {
+  const listBlogs = [
+    {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+    },
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+    },
+    {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+    },
+    {
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 12,
+    },
+    {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+    },
+  ]
+
+  test('when list has multiple blogs', () => {
+    const result = listHelper.favoriteBlog(listBlogs)
+    expect(result).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+    })
+  })
+
+  const listWithOneBlog = [
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12,
+    },
+  ]
+
+  test('when list has only one blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = blogs => {
   return blogs.reduce((acc, curr) => acc + curr.likes ?? 0, 0)
 }
 
+const favoriteBlog = blogs => {
+  const max = blogs.reduce((acc, { likes }) => (likes > acc ? likes : acc), 0)
+  return blogs.find(blog => blog.likes === max)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
