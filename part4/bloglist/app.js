fd0d84cbfc045502e743bab7753ba0eb694ef112
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-07-05 09:00:31 +0200

    4.8: Blog list tests, step1

---
+++ part4/bloglist/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,15 +1,8 @@
 const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogRouter)
+const app = require('./app')
 
 const PORT = config.PORT
 app.listen(PORT, () => {
