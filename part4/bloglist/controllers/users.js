0d24f4174d9623c11ec0340081542505dff06fb1
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-07-21 10:56:47 +0200

    4.16*: bloglist expansion, step4

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,15 @@ usersRouter.get('/', async (req, res) => {
 usersRouter.post('/', async (req, res) => {
   const body = req.body
 
+  if (!body.password) {
+    return res.status(400).json({ error: 'Password is required' })
+  }
+  if (body.password.length < 3) {
+    return res
+      .status(400)
+      .json({ error: 'Password must have at least 3 characters' })
+  }
+
   const saltRounds = 10
   const password = await bcrypt.hash(body.password, saltRounds)
 
@@ -19,8 +28,12 @@ usersRouter.post('/', async (req, res) => {
     password,
   })
 
-  const savedUser = await user.save()
-  res.json(savedUser)
+  try {
+    const savedUser = await user.save()
+    res.json(savedUser)
+  } catch (e) {
+    res.status(400).json({ error: e.message })
+  }
 })
 
 module.exports = usersRouter
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true,
+  },
   name: String,
-  password: String,
+  password: {
+    type: String,
+    required: true,
+  },
 })
+
+userSchema.plugin(uniqueValidator)
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
