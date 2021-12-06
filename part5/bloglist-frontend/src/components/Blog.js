204d72d395059ccc649419aa9f327e2378acd5e2
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-12-06 15:22:01 +0100

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showingMore, setShowingMore] = useState(false)
+
+  const toggleView = () => setShowingMore(prev => !prev)
+
+  return (
+    <div className='blog'>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleView}>{showingMore ? 'hide' : 'view'}</button>
+      </div>
+      {showingMore && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog?.user?.name}</div>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
