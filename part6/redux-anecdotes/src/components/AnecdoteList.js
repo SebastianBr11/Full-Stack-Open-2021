244034d4e5009b5a26eb78be67349c2ff5c6e12d
Sebastian Breit <flockean11@gmail.com>
2021-12-20 07:52:57 +0000

    6.10 Better anecdotes, step8

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { orderByVotes, voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-	const anecdotes = useSelector(state => orderByVotes(state))
+	const anecdotes = useSelector(state => orderByVotes(state.anecdotes))
 	const dispatch = useDispatch()
 
 	const vote = id => {
