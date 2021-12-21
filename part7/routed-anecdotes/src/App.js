6d445c1f6777980c66b384b00f9f3b173dad5bcb
Sebastian Breit <flockean11@gmail.com>
2021-12-21 06:51:59 +0000

    7.3: routed anecdotes, step3

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
-import { Switch, Route, Link, useRouteMatch } from 'react-router-dom'
+import {
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+	useHistory,
+} from 'react-router-dom'
 
 const Menu = () => {
 	const padding = {
@@ -74,6 +80,8 @@ const CreateNew = props => {
 	const [author, setAuthor] = useState('')
 	const [info, setInfo] = useState('')
 
+	const history = useHistory()
+
 	const handleSubmit = e => {
 		e.preventDefault()
 		props.addNew({
@@ -82,6 +90,7 @@ const CreateNew = props => {
 			info,
 			votes: 0,
 		})
+		history.push('/')
 	}
 
 	return (
@@ -163,6 +172,8 @@ const App = () => {
 	const addNew = anecdote => {
 		anecdote.id = (Math.random() * 10000).toFixed(0)
 		setAnecdotes(anecdotes.concat(anecdote))
+		setNotification(`a new anecdote ${anecdote.content} created!`)
+		setTimeout(() => setNotification(''), 10000)
 	}
 
 	const anecdoteById = id => anecdotes.find(a => a.id === id)
@@ -182,6 +193,7 @@ const App = () => {
 		<div>
 			<h1>Software anecdotes</h1>
 			<Menu />
+			{!!notification && <div>{notification}</div>}
 			<Switch>
 				<Route path='/about'>
 					<About />
