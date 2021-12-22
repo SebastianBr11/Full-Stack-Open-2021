a7d194d6fec259298839d0588e89eb49a0d1c8f4
Sebastian Breit <flockean11@gmail.com>
2021-12-22 06:40:20 +0000

    fixed issue where update doesn't return object with user populated

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,76 +3,80 @@ const Blog = require('../models/blog')
 const { userExtractor } = require('../utils/middleware')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+	response.json(blogs)
 })
 
 blogRouter.post('/', userExtractor, async (request, response) => {
-  const newBlog = { ...request.body }
-  if (!newBlog.likes) {
-    newBlog.likes = 0
-  }
+	const newBlog = { ...request.body }
+	if (!newBlog.likes) {
+		newBlog.likes = 0
+	}
 
-  console.log('new blog', newBlog)
+	console.log('new blog', newBlog)
 
-  if (!newBlog.title && !newBlog.url) {
-    return response.status(400).json({ error: 'Missing Title and Url' })
-  }
+	if (!newBlog.title && !newBlog.url) {
+		return response.status(400).json({ error: 'Missing Title and Url' })
+	}
 
-  const user = request.user
+	const user = request.user
 
-  newBlog.user = user._id
+	newBlog.user = user._id
 
-  const blog = new Blog(newBlog)
-  const result = await blog.save()
+	const blog = new Blog(newBlog)
+	const result = await blog.save()
 
-  user.blogs = user.blogs.concat(result._id)
-  await user.save()
+	user.blogs = user.blogs.concat(result._id)
+	await user.save()
 
-  response.status(201).json(result)
+	response.status(201).json(result)
 })
 
 blogRouter.delete('/:id', userExtractor, async (req, res) => {
-  const exists = await Blog.exists({ _id: req.params.id }).catch(err => err)
+	const exists = await Blog.exists({ _id: req.params.id }).catch(err => err)
 
-  if (exists?.message) {
-    return res.status(400).json({ error: exists.message }).end()
-  }
+	if (exists?.message) {
+		return res.status(400).json({ error: exists.message }).end()
+	}
 
-  if (!exists) {
-    return res.status(404).end()
-  }
+	if (!exists) {
+		return res.status(404).end()
+	}
 
-  const user = req.user
+	const user = req.user
 
-  const blog = await Blog.findById(req.params.id)
+	const blog = await Blog.findById(req.params.id)
 
-  if (blog.user.toString() !== user._id.toString()) {
-    return res.status(401).json({ error: 'No user permissions' })
-  }
+	if (blog.user.toString() !== user._id.toString()) {
+		return res.status(401).json({ error: 'No user permissions' })
+	}
 
-  await Blog.findByIdAndDelete(req.params.id)
-  res.status(204).end()
+	await Blog.findByIdAndDelete(req.params.id)
+	res.status(204).end()
 })
 
 blogRouter.put('/:id', async (req, res) => {
-  const post = {
-    likes: req.body.likes,
-  }
+	const post = {
+		likes: req.body.likes,
+	}
 
-  const newPost = await Blog.findByIdAndUpdate(req.params.id, post, {
-    new: true,
-  }).catch(err => err)
+	const newPost = await Blog.findByIdAndUpdate(req.params.id, post, {
+		new: true,
+	})
+		.populate('user', { username: 1, name: 1 })
+		.catch(err => err)
 
-  if (newPost?.message) {
-    return res.status(400).json({ error: newPost.message }).end()
-  }
+	if (newPost?.message) {
+		return res.status(400).json({ error: newPost.message }).end()
+	}
 
-  if (!newPost) {
-    return res.status(404).end()
-  }
+	if (!newPost) {
+		return res.status(404).end()
+	}
 
-  res.json(newPost)
+	console.log('like post', newPost)
+
+	res.json(newPost)
 })
 
 module.exports = blogRouter
