94312565f7dc586f055095f4f712eb65ac95707a
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-12-24 15:25:10 +0100

    8.6: Adding a book

--- part8/backend/index.js
+++ part8/backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
 	{
@@ -110,6 +111,15 @@ const typeDefs = gql`
 		allBooks(author: String, genre: String): [Book!]!
 		allAuthors: [Author!]!
 	}
+
+	type Mutation {
+		addBook(
+			title: String!
+			author: String!
+			published: Int!
+			genres: [String!]!
+		): Book
+	}
 `
 
 const resolvers = {
@@ -139,6 +149,18 @@ const resolvers = {
 			return []
 		},
 		allAuthors: () => authors,
+	},
+
+	Mutation: {
+		addBook: (root, args) => {
+			if (!authors.some(a => a.name === args.author)) {
+				authors.push({ name: args.author, born: null, id: uuid() })
+			}
+			const newBook = { ...args, id: uuid() }
+			books.push(newBook)
+
+			return newBook
+		},
 	},
 
 	Author: {
