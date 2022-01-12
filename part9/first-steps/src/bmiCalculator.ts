152f778967be08a768a0c786ce4f974f4e2c0abb
Sebastian Breit <flockean11@gmail.com>
2022-01-12 07:28:03 +0000

    9.3 Command line

--- part9/first-steps/src/bmiCalculator.ts
+++ part9/first-steps/src/bmiCalculator.ts
@@ -1,15 +1,38 @@
-type BmiCalc = (height: number, weight: number) => string
+type BmiCalc = (height: number, weight: number) => string;
+
+type ParseArgsBMI = (args: Array<string>) => { height: number; weight: number };
+
+const parseArgumentsBMI: ParseArgsBMI = args => {
+	if (args.length < 4) throw new Error('Not enough arguments');
+	if (args.length > 4) throw new Error('Too many arguments');
+
+	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+		return {
+			height: Number(args[2]),
+			weight: Number(args[3]),
+		};
+	}
+
+	throw new Error('Provided values were not numbers!');
+};
 
 const calculateBmi: BmiCalc = (height, weight) => {
-	const bmi = weight / (height / 100) ** 2
+	const bmi = weight / (height / 100) ** 2;
 	if (bmi < 18.5) {
-		return 'Underweight'
+		return 'Underweight';
 	} else if (bmi < 25) {
-		return 'Normal (healthy weight)'
+		return 'Normal (healthy weight)';
 	} else if (bmi < 30) {
-		return 'Overweight'
+		return 'Overweight';
 	}
-	return 'Obese'
-}
+	return 'Obese';
+};
 
-console.log(calculateBmi(180, 74))
+try {
+	const { height, weight } = parseArgumentsBMI(process.argv);
+	console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+	if (error instanceof Error) {
+		console.log(error.message);
+	}
+}
--- part9/first-steps/src/exerciseCalculator.ts
+++ part9/first-steps/src/exerciseCalculator.ts
@@ -1,25 +1,47 @@
-interface Result {
-	periodLength: number
-	trainingDays: number
-	success: boolean
-	rating: number
-	ratingDescription: string
-	target: number
-	average: number
-}
-type CalcExercises = (dailyHours: number[], dayTarget: number) => Result
+type ParseArgsEx = (args: Array<string>) => {
+	dailyHours: number[];
+	dayTarget: number;
+};
 
-type GetRating = (average: number, dayTarget: number) => number
-type GetRatingDesc = (rating: number) => string
+const parseArgumentsEx: ParseArgsEx = args => {
+	if (args.length < 4) throw new Error('Not enough arguments');
+	const dailyHours = args.slice(3);
+
+	if (
+		!isNaN(Number(args.at(2))) &&
+		dailyHours.every(day => !isNaN(Number(day)))
+	) {
+		return {
+			dailyHours: dailyHours.map(day => Number(day)),
+			dayTarget: Number(args.at(2)),
+		};
+	}
+
+	throw new Error('Provided values were not numbers!');
+};
+
+interface Result {
+	periodLength: number;
+	trainingDays: number;
+	success: boolean;
+	rating: number;
+	ratingDescription: string;
+	target: number;
+	average: number;
+}
+type CalcExercises = (dailyHours: number[], dayTarget: number) => Result;
+
+type GetRating = (average: number, dayTarget: number) => number;
+type GetRatingDesc = (rating: number) => string;
 
 const calculateExercises: CalcExercises = (dailyHours, dayTarget) => {
-	const periodLength = dailyHours.length
-	const trainingDays = dailyHours.filter(day => day !== 0).length
-	const success = dailyHours.every(day => day >= dayTarget)
+	const periodLength = dailyHours.length;
+	const trainingDays = dailyHours.filter(day => day !== 0).length;
+	const success = dailyHours.every(day => day >= dayTarget);
 	const average =
-		dailyHours.reduce((acc, val) => acc + val, 0) / dailyHours.length
-	const rating = getRating(average, dayTarget)
-	const ratingDescription = getRatingDescription(rating)
+		dailyHours.reduce((acc, val) => acc + val, 0) / dailyHours.length;
+	const rating = getRating(average, dayTarget);
+	const ratingDescription = getRatingDescription(rating);
 
 	return {
 		periodLength,
@@ -29,30 +51,37 @@ const calculateExercises: CalcExercises = (dailyHours, dayTarget) => {
 		ratingDescription,
 		target: dayTarget,
 		average,
-	}
-}
+	};
+};
 
 const getRating: GetRating = (average, dayTarget) => {
-	const percentage = average / dayTarget
+	const percentage = average / dayTarget;
 	if (percentage >= 1) {
-		return 3
+		return 3;
 	} else if (percentage > 0.75) {
-		return 2
+		return 2;
 	}
-	return 1
-}
+	return 1;
+};
 
 const getRatingDescription: GetRatingDesc = rating => {
 	switch (rating) {
 		case 3:
-			return 'Very well done'
+			return 'Very well done';
 		case 2:
-			return 'not too bad but could be better'
+			return 'not too bad but could be better';
 		case 1:
-			return 'not very good'
+			return 'not very good';
 		default:
-			return 'invalid rating'
+			return 'invalid rating';
+	}
+};
+
+try {
+	const { dailyHours, dayTarget } = parseArgumentsEx(process.argv);
+	console.log(calculateExercises(dailyHours, dayTarget));
+} catch (error) {
+	if (error instanceof Error) {
+		console.log(error.message);
 	}
 }
-
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
