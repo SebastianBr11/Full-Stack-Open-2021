ded42b4c71dfa60440a9a162eee2b3c23179fc55
Sebastian Breit <flockean11@gmail.com>
2022-01-12 07:07:48 +0000

    9.2 Exercise calculator

---
+++ part9/first-steps/src/exerciseCalculator.ts
@@ -0,0 +1,58 @@
+interface Result {
+	periodLength: number
+	trainingDays: number
+	success: boolean
+	rating: number
+	ratingDescription: string
+	target: number
+	average: number
+}
+type CalcExercises = (dailyHours: number[], dayTarget: number) => Result
+
+type GetRating = (average: number, dayTarget: number) => number
+type GetRatingDesc = (rating: number) => string
+
+const calculateExercises: CalcExercises = (dailyHours, dayTarget) => {
+	const periodLength = dailyHours.length
+	const trainingDays = dailyHours.filter(day => day !== 0).length
+	const success = dailyHours.every(day => day >= dayTarget)
+	const average =
+		dailyHours.reduce((acc, val) => acc + val, 0) / dailyHours.length
+	const rating = getRating(average, dayTarget)
+	const ratingDescription = getRatingDescription(rating)
+
+	return {
+		periodLength,
+		trainingDays,
+		success,
+		rating,
+		ratingDescription,
+		target: dayTarget,
+		average,
+	}
+}
+
+const getRating: GetRating = (average, dayTarget) => {
+	const percentage = average / dayTarget
+	if (percentage >= 1) {
+		return 3
+	} else if (percentage > 0.75) {
+		return 2
+	}
+	return 1
+}
+
+const getRatingDescription: GetRatingDesc = rating => {
+	switch (rating) {
+		case 3:
+			return 'Very well done'
+		case 2:
+			return 'not too bad but could be better'
+		case 1:
+			return 'not very good'
+		default:
+			return 'invalid rating'
+	}
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
